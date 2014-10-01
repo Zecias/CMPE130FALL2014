9a47dee29c1559922c9fe6de4e5492660a007a4f
Brandon Zhen <bzijiez@gmail.com>
2014-09-30 23:58:51 -0700

    Project Details and TODO List

--- src/cmpe130main.cpp
+++ src/cmpe130main.cpp
@@ -1,15 +1,43 @@
-//============================================================================
-// Name        : cmpe130main.cpp
-// Author      : 
-// Version     :
-// Copyright   : 
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+/*<3**return*void*~*-std=c++0x*~*return*void*~*-std=c++11*~*return*void**<3*\
+* Name        : <Untitled>													*
+* Authors     : Brandon Zhen, Edward Huang, Kevin Lai						*
+* Version     : 0.0.0														*
+* Copyright   : Borders © BZhen 2014										*
+* Description : 															*
+\*<3**return*void*~*-pthread<3*~*return*void*~*>linux_gcc*~*return*void**<3*/
+
+/*																			*\
+TODO:	Project Abstract due Thursday, October 9
+			(a) Title of your project
+
+			(b) Names and student IDs of the members
+					Brandon Zhen
+					Edward Huang
+					Kevin Lai
+			(C) One paragraph describing what you will do in the project
+
+			(d) A weekly schedule of what you will accomplish
+
+\* 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 		*/
+
+/*																			*\
+TODO:	Proposed Project and Ideas
+			SPAM Filter, Automated Detection and Deletion
+				1. Gmail Java API will be used to read SPAM tagged emails in
+					target gmail account and send email body string to 2.
+				2. C++ executable will be created to parse whether email
+					body strings are deletable and return results back to 1.
+				1. Gmail Java API will either delete are keep SPAM based on
+					the C++ execuable parser's results.
+			Kevin determines details to be inadequate.
+			We will start planning things out this Friday.
+\*																			*/
 
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
-int main() {
-	cout << "" << endl; // prints 
-	return 0;
+int main(int argc, char* argv[]) {
+	cout << __cplusplus << endl;
+	return EXIT_SUCCESS;
 }
