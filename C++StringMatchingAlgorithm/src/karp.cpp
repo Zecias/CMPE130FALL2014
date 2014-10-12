16b9a8deb55c0e504b06fd44fee87a7f98cf9dd6
Brandon Zhen <bzijiez@gmail.com>
2014-10-12 16:08:25 -0700

    Simple Tested and Works Code

--- C++StringMatchingAlgorithm/src/karp.cpp
+++ C++StringMatchingAlgorithm/src/karp.cpp
@@ -25,3 +25,53 @@ unsigned rankString(std::string input_string, keys search_keys) {/*
 		REHASH(CURRENT_HASH - PREVIOUS_HASH + NEXT_HASH)
 	return RANK
 */}
+
+
+//#define hashlimit 3
+//
+//void main_1() {
+//	vector<string> keywords;			//Declarations
+//	vector<signed> hashes;
+//
+//	ifstream file("key.txt");			//Get Keywords
+//	while (!file.eof()) {
+//		string str;
+//		getline(file, str);
+//		keywords.push_back(str);
+//	}
+//
+//	for (auto spamgods : keywords) {	//Hash Keywords
+//		signed hash = 0;
+//		for (int i = 0; i < hashlimit; i++)
+//			hash += spamgods[i];
+//		hashes.push_back(hash);			//Shows Body Text
+//	}
+//
+//	file.close();//Mush
+//	file.open("doc.txt");				//Get Body Text
+//	string bodytext;
+//	getline(file, bodytext);
+//	cout << bodytext << endl;
+//
+//	signed dochash = 0;					//Initial Hash of Body Text
+//	for (int i = 0; i < hashlimit; i++)
+//		dochash += bodytext[i];
+//
+//	for (unsigned i = hashlimit; i < bodytext.length(); i++) {
+//		for (unsigned j = 0; j < hashes.size(); j++) {
+//			if (dochash == hashes[j]) {	//Possible Match, check keyword
+//				if (!bodytext.compare(i-hashlimit, keywords[j].length(), keywords[j])) {
+//					cout << "MATCHED" << endl;
+//					exit(EXIT_SUCCESS);
+//				}
+//			}
+//		}								//Realtime Hash Updating
+//		dochash = dochash - bodytext[i-hashlimit] + bodytext[i];
+//	}
+//
+//	for (unsigned i = 0; i < keywords.size(); i++)	//Shows Keywords On Fail
+//		cout << keywords[i] << " " << hashes[i] << endl;
+//
+//	cout << "NO MATCHES FOUND" << endl;	//Failure
+//	exit(EXIT_FAILURE);
+//}
