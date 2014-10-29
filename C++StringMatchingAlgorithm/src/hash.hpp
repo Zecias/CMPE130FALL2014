e19c1a3becc3507edd097d23c0ab2f4c0cb28d1d
Brandon Zhen <bzijiez@gmail.com>
2014-10-28 17:39:48 -0700

    Perfect Hash Function Created (Supports up to 6 character length)

--- C++StringMatchingAlgorithm/src/hash.hpp
+++ C++StringMatchingAlgorithm/src/hash.hpp
@@ -11,6 +11,8 @@
 
 #include <string>
 
+typedef unsigned p_hash_t;
+
 /**
  * @param	text	The string to hash completely
  * @return	An 8 bit wide hash between 0 - 127
@@ -37,6 +39,21 @@ signed char hash(std::string text, unsigned begin, unsigned end) {
  */
 signed char update_hash(signed char hash, char append_char, char dehash_char) {
 	return 0;
+}
+
+/**
+ * This function creates a perfect alphabet hash such that:
+ * Example: p_hash("ab") != p_hash("ba")
+ * Example: p_hash("Ab") == p_hash("aB")
+ * Usage: [ update_p_hash(empty_hash, new_character, 6); ]
+ *
+ * @param	hash	The <hash> to append <characters> to
+ * @param	append	The single <character> from aA~zZ to append to <hash>
+ * @param	len		The max character <length> the <hash> contains [max == 6]
+ * @return	A 30 bit wide hash containing the hash of <length> characters
+ */
+void p_hash(p_hash_t& hash, char append, char len = 6) {
+	hash = ((hash << 5) | (append & 0b11111)) & 07777777777;
 }
 
 #endif /* HASH_HPP_ */
