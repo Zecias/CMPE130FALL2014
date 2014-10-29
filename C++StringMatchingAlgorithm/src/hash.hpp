67649610866e02ebe760b4e02666298e2cf8c950
Brandon Zhen <bzijiez@gmail.com>
2014-10-28 18:24:25 -0700

    1.Perfect Hash Function edited to hold 1-6 alpha_char instead of just 6 2.Long Perfect Hash Function added to hold 1-12 alpha_char just in case

--- C++StringMatchingAlgorithm/src/hash.hpp
+++ C++StringMatchingAlgorithm/src/hash.hpp
@@ -11,41 +11,14 @@
 
 #include <string>
 
-typedef unsigned p_hash_t;
-
-/**
- * @param	text	The string to hash completely
- * @return	An 8 bit wide hash between 0 - 127
- */
-signed char hash(std::string text) {
-	return 0;
-}
-
-/**
- * @param	text	The substring to hash
- * @param	begin	The first index of the substring
- * @param	end		The last index of the substring
- * @return	An 8 bit wide hash between 0 - 127
- */
-signed char hash(std::string text, unsigned begin, unsigned end) {
-	return 0;
-}
-
-/**
- * @param	hash		The <hash> to update
- * @param	append_char	The character to hash into <hash>
- * @param	dehash_char	The character to hash out of <hash>
- * @return	An 8 bit wide hash between 0 - 127
- */
-signed char update_hash(signed char hash, char append_char, char dehash_char) {
-	return 0;
-}
+typedef unsigned long		p_hash_t;
+typedef unsigned long long	lp_hash_t;
 
 /**
  * This function creates a perfect alphabet hash such that:
  * Example: p_hash("ab") != p_hash("ba")
  * Example: p_hash("Ab") == p_hash("aB")
- * Usage: [ update_p_hash(empty_hash, new_character, 6); ]
+ * Usage: [ p_hash(empty_hash, 'a', 3); ]
  *
  * @param	hash	The <hash> to append <characters> to
  * @param	append	The single <character> from aA~zZ to append to <hash>
@@ -53,7 +26,50 @@ signed char update_hash(signed char hash, char append_char, char dehash_char) {
  * @return	A 30 bit wide hash containing the hash of <length> characters
  */
 void p_hash(p_hash_t& hash, char append, char len = 6) {
-	hash = ((hash << 5) | (append & 0b11111)) & 07777777777;
+	hash = ((hash << 5) | (append & 0b11111)) & (07777777777 >> (6-len)*5);
 }
+
+/**
+ * This function creates a perfect alphabet hash such that:
+ * Example: lp_hash("ab") != lp_hash("ba")
+ * Example: lp_hash("Ab") == lp_hash("aB")
+ * Usage: [ lp_hash(empty_hash, 'a', 9); ]
+ *
+ * @param	hash	The <hash> to append <characters> to
+ * @param	append	The single <character> from aA~zZ to append to <hash>
+ * @param	len		The max character <length> the <hash> contains [max == 12]
+ * @return	A 60 bit wide hash containing the hash of <length> characters
+ */
+void lp_hash(lp_hash_t& hash, char append, char len = 12) {
+	hash = ((hash << 5) | (append & 0b11111)) & (0xFFFFFFFFFFFFFFF >> (6-len)*5);
+}
+
+///**
+// * @param	text	The string to hash completely
+// * @return	An 8 bit wide hash between 0 - 127
+// */
+//signed char hash(std::string text) {
+//	return 0;
+//}
+//
+///**
+// * @param	text	The substring to hash
+// * @param	begin	The first index of the substring
+// * @param	end		The last index of the substring
+// * @return	An 8 bit wide hash between 0 - 127
+// */
+//signed char hash(std::string text, unsigned begin, unsigned end) {
+//	return 0;
+//}
+//
+///**
+// * @param	hash		The <hash> to update
+// * @param	append_char	The character to hash into <hash>
+// * @param	dehash_char	The character to hash out of <hash>
+// * @return	An 8 bit wide hash between 0 - 127
+// */
+//signed char update_hash(signed char hash, char append_char, char dehash_char) {
+//	return 0;
+//}
 
 #endif /* HASH_HPP_ */
