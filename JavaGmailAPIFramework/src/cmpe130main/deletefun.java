b2cedae38fe876683b87efdd9d90fdf514965ca2
Kevin Lai <kevinlai249@gmail.com>
2014-10-30 15:55:49 -0700

    To automatically delete SPAM

---
+++ JavaGmailAPIFramework/src/cmpe130main/deletefun.java
@@ -0,0 +1,26 @@
+package cmpe130main;
+import com.google.api.services.gmail.Gmail;
+
+import java.io.IOException;
+
+
+public class deletefun {
+
+  /**
+   * Immediately and permanently deletes the specified thread. This operation cannot
+   * be undone. Prefer threads.trash instead.
+   *
+   * @param service Authorized Gmail API instance.
+   * @param userId User's email address. The special value "me"
+   * can be used to indicate the authenticated user.
+   * @param threadId ID of Thread to delete.
+   * @throws IOException
+   */
+  public static void deleteThread(Gmail service, String userId, String threadId)
+      throws IOException {
+    service.users().threads().delete(userId, threadId).execute();
+    System.out.println("Thread with id: " + threadId + " deleted successfully.");
+  }
+
+
+}
