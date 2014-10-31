b7c9703cd856fe524e88890c07fb42d9b17e215e
Brandon Zhen <bzijiez@gmail.com>
2014-10-30 18:22:53 -0700

    Removed previous Hash Comments for Clarification

--- C++StringMatchingAlgorithm/src/hash.hpp
+++ C++StringMatchingAlgorithm/src/hash.hpp
@@ -42,34 +42,4 @@ void lp_hash(lp_hash_t& hash, char append, char len = 12) {
 	hash = ((hash << 5) | (append & 0b11111)) & (0xFFFFFFFFFFFFFFF >> (6-len)*5);
 }
 
-//#include <string>
-//
-///**
-// * @param	text	The string to hash completely
-// * @return	An 8 bit wide hash between 0 - 127
-// */
-//signed char hash(std::string text) {
-//	return 0;
-//}
-//
-///**
-// * @param	text	The substring to hash
-// * @param	begin	The first index of the substring
-// * @param	end		The last index of the substring
-// * @return	An 8 bit wide hash between 0 - 127
-// */
-//signed char hash(std::string text, unsigned begin, unsigned end) {
-//	return 0;
-//}
-//
-///**
-// * @param	hash		The <hash> to update
-// * @param	append_char	The character to hash into <hash>
-// * @param	dehash_char	The character to hash out of <hash>
-// * @return	An 8 bit wide hash between 0 - 127
-// */
-//signed char update_hash(signed char hash, char append_char, char dehash_char) {
-//	return 0;
-//}
-
 #endif /* HASH_HPP_ */
