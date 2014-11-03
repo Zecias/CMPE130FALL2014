a356a5b8d95dc19f95e5ed69a909c889003117f9
Kevin Lai <kevinlai249@gmail.com>
2014-11-03 15:22:42 -0800

    Modified Hash Function
    This is the hash function in JAVA. I was able to convert the c++ version
    to JAVA. The tested result was identical to the c++ version so it should
    be correct. -Kevin Lai

---
+++ JavaGmailAPIFramework/src/cmpe130main/HashFunction.java
@@ -0,0 +1,50 @@
+package cmpe130main;
+//import java.math.BigInteger;		// Java's Equivalent to long long
+
+public class HashFunction{
+	
+	public static class hash_t{
+		public long hasher;
+		
+		public hash_t(){
+			hasher = 0;
+		}
+		
+	}
+	
+	
+	private static int hash_t_len_max = 6;
+//	private static int lhash_t_len_max = 12;
+	
+	private static long hash_t_mask_max = 1073741823;
+	
+	public static void hash(hash_t hash, char append, int len) {
+		hash.hasher = ((hash.hasher << 5) | (append & 0b11111))
+			& (hash_t_mask_max >> (hash_t_len_max - len) * 5);
+	}
+	
+	
+//	public static void lhash(hash_t hash, char append, int len) {
+//		hash = ((hash << 5) | (append & 0b11111))
+//			& (lhash_t_mask_max >> (lhash_t_len_max - len) * 5);
+//	}
+	
+	
+	public static void main(String[] args){
+		
+		hash_t hashed = new hash_t();
+		
+		hash(hashed, 'A', 6);
+		hash(hashed, 'b', 6);
+		hash(hashed, 'C', 6);
+		hash(hashed, 'd', 6);
+		hash(hashed, 'E', 6);
+		hash(hashed, 'f', 6);
+		hash(hashed, 'G', 6);
+		
+		System.out.println(hashed.hasher);
+		
+			
+	}
+	
+}
