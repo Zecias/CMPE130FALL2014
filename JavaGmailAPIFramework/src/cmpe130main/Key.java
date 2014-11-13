9b0b448ffe6ba991359bcbeecc6219b979999d73
Kevin Lai <kevinlai249@gmail.com>
2014-11-12 18:44:14 -0800

    Keys.java - Still Unfinished - On Hiatus
    On hiatus because we might not have enough time to finish converting all
    the code to java. As such, we should finish testing the functions in c++
    before we try JAVA and gmail api.
    - Updated the keys.java

--- JavaGmailAPIFramework/src/cmpe130main/Key.java
+++ JavaGmailAPIFramework/src/cmpe130main/Key.java
@@ -1,5 +1,8 @@
 package cmpe130main;
 import java.util.Vector;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
 
 public class Key extends HashFunction{
 
@@ -21,9 +24,9 @@ public class Key extends HashFunction{
 	 */
 	
 	public static class keys {
-		vector<string> keyword[32];
-		vector<rank_t> rank[32];
-		vector<hash_t> hash[32];
+		Vector<String> keyword = new Vector<String>(32);
+		Vector<rank_t> rank = new Vector<rank_t>(32);
+		Vector<hash_t> hash = new Vector<hash_t>(32);
 		int maxlen = hash_t_len_max;
 	};
 	
@@ -40,7 +43,85 @@ public class Key extends HashFunction{
 	 * @return	true on load successful
 	 * @return	false on load failure
 	 */
-	bool load(keys keyset, char const* filename);
+	Boolean load(keys keyset, String filename) {
+		
+		public String[] OpenFile() throws IOException{
+			FileReader in = new FileReader(filename);
+			BufferedReader textReader = new BufferedReader(in);
+			
+			String line;
+			while((line = textReader.readLine()) != null){
+				String a;
+				a = textReader.readLine();
+				
+				int delimination_index = a.indexOf('|');
+				int hash_index = a.charAt(delimination_index + 1) & 31;
+				int word_length = a.length() - delimination_index - 2;
+				
+				keyset.keyword.add(hash_index,a.substring(
+						delimination_index + 1, word_length));
+				
+				
+				/* this needs to be fixed somehow.
+				 * 
+				*  The problem here is because Brandon likes defining his own types
+				*  Because he wanted to be complex and make rank_t instead of just using long,
+				*  the error below is present.
+				*/
+				keyset.rank.add(hash_index, Integer.parseInt(a.substring(0, delimination_index)));
+				
+				if (word_length < keyset.maxlen){
+					keyset.maxlen = word_length;}
+			}
+			
+			textReader.close();
+			
+		}
+		
+//	
+//		
+//		if (in.fail())
+//			return false;
+//		while (!in.eof()) {
+//			std::string str;
+//			getline(in, str);
+//			unsigned delimination_index = str.find('|');
+//			unsigned hash_index = str[delimination_index + 1];
+//			unsigned word_length = str.length() - delimination_index - 2;
+//			keyset.keyword[hash_index].push_back(str.substr(
+//					delimination_index + 1, word_length));
+//			keyset.rank[hash_index].push_back(std::atoi(
+//					str.substr(0, delimination_index).c_str()));
+//			if (word_length < keyset.maxlen)
+//				keyset.maxlen = word_length;
+//		}
+//		for (unsigned i = 0; i < 32; i++) {
+//			/* Bubbly Sort */
+//			if (keyset.keyword[i].size() > 1)
+//				for (unsigned top = 0,
+//						bot = keyset.keyword[i].size() - 1,
+//						next_top = keyset.keyword[i].size() - 1;
+//						top < keyset.keyword[i].size() - 1;
+//						top = next_top,
+//						next_top = bot = keyset.keyword[i].size() - 1)
+//					for (; top < bot; bot--)
+//						if (keyset.keyword[i][bot - 1].compare(
+//								keyset.keyword[i][bot]) > 0)
+//							std::swap(
+//									keyset.keyword[i][bot],
+//									keyset.keyword[i][bot - 1]),
+//							std::swap(
+//									keyset.rank[i][bot],
+//									keyset.rank[i][bot - 1]),
+//							next_top = bot;
+//			keyset.hash[i].resize(keyset.keyword[i].size());
+//			for (unsigned v = 0; v < keyset.keyword[i].size(); v++)
+//				for (unsigned s = 0; s < keyset.maxlen; s++)
+//					hash(keyset.hash[i][v], keyset.keyword[i][v][s], keyset.maxlen);
+//		}
+//		return true;
+//	}
+
 
 	/**
 	 * Binary Hash Table Search
